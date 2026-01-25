a78e4c40163ef94a27d7ce814a63392c7c4c0603
Naveen Ayalla <naveenayalla1@gmail.com>
2026-01-25 10:41:30 -0800

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "glue_job_name" {
-  value = aws_glue_job.this.name
+  value = aws_glue_job.job.name
+}
+
+output "glue_role_arn" {
+  value = aws_iam_role.glue_role.arn
+}
+
+output "glue_script_s3" {
+  value = "s3://${var.artifacts_bucket}/${var.glue_script_key}"
 }
