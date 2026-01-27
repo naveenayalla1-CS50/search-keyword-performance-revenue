7183fd374af424b3e110dbae15c88ce8f563ca85
Naveen Ayalla <naveenayalla1@gmail.com>
2026-01-27 08:03:50 -0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "naveen-terraform-state-821891894533"
-    key            = "search-keyword-performance/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
