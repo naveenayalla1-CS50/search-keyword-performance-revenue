f3c90f30c978ad09fc64ecacaf60eaafd205bdb7
Naveen Ayalla <naveenayalla1@gmail.com>
2026-01-26 20:50:15 -0800

    Update glue_job.py

--- glue_job.py
+++ glue_job.py
@@ -1,6 +1,7 @@
 """
 AWS Glue PySpark Job: Search Keyword Performance from Adobe Analytics Hit-Level Data
 This job:
+
 - Reads tab-delimited hit data from S3
 - Processes it using SearchKeywordPerformanceApp
 - Writes sorted tab-delimited output with date prefix
