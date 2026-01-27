8c74236b65bb4446b8c9e62f59cb642da32bc582
Naveen Ayalla <naveenayalla1@gmail.com>
2026-01-27 08:53:08 -0800

    Update artifacts.tf

--- terraform/artifacts.tf
+++ terraform/artifacts.tf
@@ -1,26 +1,30 @@
-# These files must exist on the machine running terraform (GitHub Actions runner)
 variable "local_glue_script_path" {
   type        = string
   description = "Local path to glue script"
-  default     = "../glue_job.py"
+  default     = null
 }
 
 variable "local_extra_py_zip_path" {
   type        = string
   description = "Local path to glue helper zip"
-  default     = "../dist/glue-helper.zip"
+  default     = null
+}
+
+locals {
+  glue_script_path   = coalesce(var.local_glue_script_path, "${path.module}/../glue_job.py")
+  extra_py_zip_path  = coalesce(var.local_extra_py_zip_path, "${path.module}/../dist/glue-helper.zip")
 }
 
 resource "aws_s3_object" "glue_script" {
   bucket = var.artifacts_bucket
   key    = var.glue_script_key
-  source = var.local_glue_script_path
-  etag   = filemd5(var.local_glue_script_path)
+  source = local.glue_script_path
+  etag   = filemd5(local.glue_script_path)
 }
 
 resource "aws_s3_object" "extra_py_zip" {
   bucket = var.artifacts_bucket
   key    = var.extra_py_files_key
-  source = var.local_extra_py_zip_path
-  etag   = filemd5(var.local_extra_py_zip_path)
+  source = local.extra_py_zip_path
+  etag   = filemd5(local.extra_py_zip_path)
 }
